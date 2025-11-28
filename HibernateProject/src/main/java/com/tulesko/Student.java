1da687b11c4163d99d2dda498f39cd7826bb676f
PCnslt <info@pcnslt.com>
2025-11-28 17:21:24 -0500

    Add Basic Hibernate create table example.  Reorganize JDBCCourse folder.

---
+++ HibernateProject/src/main/java/com/tulesko/Student.java
@@ -0,0 +1,48 @@
+package com.tulesko;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Student {
+
+    @Id
+    private int rollNo;
+    private String sName;
+    private int sAge;
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public int getsAge() {
+        return sAge;
+    }
+
+    public void setsAge(int sAge) {
+        this.sAge = sAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", sName='" + sName + '\'' +
+                ", sAge=" + sAge +
+                '}';
+    }
+}
