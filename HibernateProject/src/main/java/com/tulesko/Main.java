17a1496df11a8e2c272cba827ac720470ca22537
PCnslt <info@pcnslt.com>
2025-11-28 17:35:02 -0500

    Add Hibernate Fetch data example.

--- HibernateProject/src/main/java/com/tulesko/Main.java
+++ HibernateProject/src/main/java/com/tulesko/Main.java
@@ -1,3 +1,40 @@
+//package com.tulesko;
+//
+//import jakarta.transaction.*;
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.Transaction;
+//import org.hibernate.cfg.Configuration;
+//
+//public class Main {
+//    public static void main(String[] args) throws HeuristicRollbackException, SystemException, HeuristicMixedException, RollbackException {
+//
+//        Student s1 = new Student();
+//        s1.setsName("Adam");
+//        s1.setRollNo(104);
+//        s1.setsAge(39);
+//
+//        Configuration cfg = new Configuration();
+//        cfg.addAnnotatedClass(com.tulesko.Student.class);
+//        cfg.configure();
+//
+//        SessionFactory sf = cfg.buildSessionFactory();
+//        Session session = sf.openSession();
+//        Transaction transaction = session.beginTransaction();
+//
+//        session.persist(s1);
+//
+//        transaction.commit();
+//
+//        session.close();
+//        sf.close();
+//
+//        System.out.println(s1.toString());
+//
+//
+//    }
+//}
+
 package com.tulesko;
 
 import jakarta.transaction.*;
@@ -6,15 +43,10 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws HeuristicRollbackException, SystemException, HeuristicMixedException, RollbackException {
 
-        Student s1 = new Student();
-        s1.setsName("Adam");
-        s1.setRollNo(104);
-        s1.setsAge(39);
+        Student s2 = null;
 
         Configuration cfg = new Configuration();
         cfg.addAnnotatedClass(com.tulesko.Student.class);
@@ -23,15 +55,12 @@ public class Main {
         SessionFactory sf = cfg.buildSessionFactory();
         Session session = sf.openSession();
 
-//        Transaction transaction = (Transaction) session.beginTransaction();
-        Transaction transaction = session.beginTransaction();
-        session.persist(s1);
-        transaction.commit();
+        s2 = session.find(Student.class, 102);
 
         session.close();
         sf.close();
 
-        System.out.println(s1.toString());
+        System.out.println(s2.toString());
 
 
     }
