15371f5358f7b92bc139b5886cb15a2bd913b3c5
PCnslt <info@pcnslt.com>
2025-11-28 16:05:40 -0500

    Add Prepared Statement example.

--- JavaBasics/JDBCCourse/src/DemoJdbc.java
+++ JavaBasics/JDBCCourse/src/DemoJdbc.java
@@ -25,15 +25,23 @@ public class DemoJdbc {
 //        String sql = "select * from student";
 //        String sql = "insert into student values (2,'Anna',75)";
 //        String sql = "update student set sname='Larry' where sid=2";
-        String sql = "delete from student where sid=1";
+//        String sql = "delete from student where sid=1";
+        String sql = "insert into student values (?,?,?)";
 
         Connection con = DriverManager.getConnection(url,uname,pass);
         System.out.println("Conection established...");
-        Statement st = con.createStatement();
-//        ResultSet rs = st.executeQuery(sql);
-        boolean rs = st.execute(sql);
+//        Statement st = con.createStatement();
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setInt(1,1);
+        st.setString(2, "Shawn");
+        st.setInt(3,75);
 
-        System.out.println(rs);
+        System.out.println(st.execute());
+
+//        ResultSet rs = st.executeQuery(sql);
+//        boolean rs = st.execute(sql);
+
+//        System.out.println(rs);
 
 //        rs.next();
 //        String name = rs.getString("sname");
