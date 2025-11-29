b506489090df2a1b6491df1c41bbbaf545630d69
PCnslt <info@pcnslt.com>
2025-11-29 00:53:01 -0500

    Add SpringBootDemo2 app.

---
+++ SpringBootDemo2/src/main/java/com/tulesko/app/Computer.java
@@ -0,0 +1,7 @@
+package com.tulesko.app;
+
+public interface Computer {
+
+    void compile();
+}
+;
---
+++ SpringBootDemo2/src/main/java/com/tulesko/app/SpringBootDemo2Application.java
@@ -0,0 +1,21 @@
+package com.tulesko.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootDemo2Application {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringBootDemo2Application.class, args);
+
+		Alien obj=context.getBean(Alien.class);
+		System.out.println(obj.getAge());
+		obj.code();
+
+	}
+
+
+
+}
